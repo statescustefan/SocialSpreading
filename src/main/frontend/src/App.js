224b095f6374215483bf80394c9a30fb0357482f
statescustefan <statescustefan@gmail.com>
2020-08-24 17:35:57 +0200

    *api data in frontend *cors problem solved

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            speakers: [],
+            sessions: [],
+            isLoaded: false
+        }
+
+    }
+
+    componentDidMount() {
+
+        fetch('http://localhost:8080/api/v1/sessions')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    sessions: json,
+                    isLoaded: true,
+                })
+            }).catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+    render() {
+
+        const {isLoaded, sessions} = this.state;
+
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+            <div className="App">
+                <ul>
+                    {sessions.map(item => (
+                        <li key={item.id}>
+                            Name: {item.name} | Email: {item.email}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+
+    }
+
 }
 
 export default App;
